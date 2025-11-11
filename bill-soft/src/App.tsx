6a44e053aff6d703d347a63c14955eda4ff11716
vishal <mailtomevishalkv@gmail.com>
2025-11-11 23:05:13 +0530

    define project structure

--- bill-soft/src/App.tsx
+++ bill-soft/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Dashboard from './components/Dashboard'
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <div>
         <h1>Bill soft</h1>
+        <Dashboard />
       </div>
     </>
   )
